28ba57081abd58cb34078d95e636939cc35d69f2
Silvia Saverino <silviasave89@gmail.com>
2025-01-16 19:23:24 +0000

    add initial pages layout

---
+++ src/assets/Components/Home/Home.jsx
@@ -0,0 +1,17 @@
+import { React } from 'react'
+import './Home.css'
+import Navbar from '../Navbar/Navbar'
+
+function App() {
+  return (
+    <>
+      <div>
+        <Navbar></Navbar>
+      </div>
+    </>
+  )
+}
+
+export default App
+
+
---
+++ src/assets/Components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav>
+      <img src="" alt="" />
+      <ul>
+        <li>
+          Home
+        </li>
+        <li>
+          About
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
