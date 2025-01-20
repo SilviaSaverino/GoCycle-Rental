1e0b5ef5ab58c938be54864bb815de22c67ac839
Silvia Saverino <silviasave89@gmail.com>
2025-01-20 14:25:37 +0000

    add filter and sort buttons; Refactor CSS for improved readability and organization

--- src/Pages/Hire Bike/Bikes.jsx
+++ src/Pages/Hire Bike/Bikes.jsx
@@ -2,8 +2,7 @@ import React from "react"
 import { Link } from "react-router-dom"
 import "./Bikes.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
-
+import { faArrowUpRightFromSquare, faFilter, faSort } from '@fortawesome/free-solid-svg-icons';
 
 export default function RentBikes() {
 
@@ -20,20 +19,19 @@ export default function RentBikes() {
             <img src={bike.imageUrl} alt={bike.name} />
             <div className="bikesInfo">
                 <h3>{bike.name}</h3>
-                <p>£<span>{bike.price}</span>/day</p> 
-                <button className="bikesInfo-btn">Learn More <FontAwesomeIcon icon={faArrowUpRightFromSquare} className="arrow-icon" /></button>
+                <p>£<span>{bike.price}</span>/day</p>
+                <button className="bikesInfo-btn">Learn More <FontAwesomeIcon icon={faArrowUpRightFromSquare} className="icon" /></button>
             </div>
-            
         </div>
     ))
 
     return (
         <div className="hire-container">
-        <h1>Explore our bikes selection</h1>
-        <div className="btn-container">
-            <button>Filter</button>
-            <button>Sort</button>
-        </div>
+            <div className="top-container">
+                <h1>Explore our bikes selection</h1>
+                <button>Filter<FontAwesomeIcon icon={faFilter} className="icon" /></button>
+                <button>Sort<FontAwesomeIcon icon={faSort} className="icon" /></button>
+            </div>
             <div className="bikes-container">
                 {bikes}
             </div>
