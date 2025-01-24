7a5bcc478d677b329d73c8bdc312b46039a1e043
Silvia Saverino <silviasave89@gmail.com>
2025-01-24 15:25:28 +0000

    remove Navbar from Browser Rooter and use nested Route instead. Render Navbar from Layout.jsx and use Outlet to render the content of the website pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route, Link} from "react-router-dom"
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import "../server"
 import './App.css'
-import Navbar from './Components/Navbar/Navbar'
+// import Navbar from './Components/Navbar/Navbar'
 import HomePage from './Pages/Home/Home.jsx'
 import About from './Pages/About/About.jsx'
 import ContactUs from './Pages/Contact Us/Contact.jsx'
 import Help from './Pages/Help/Help.jsx'
 import HireBike from './Pages/Hire Bike/Bikes.jsx'
 import BikeInfo from './Pages/Hire Bike/BikeInfo.jsx'
+import Layout from './Components/Layout/Layout.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter basename="/GoCycle-Rental">
-    <Navbar />
+    {/* <Navbar /> */}
     <Routes>
-      <Route path="GoCycle-Rental/" element={<HomePage />} />
-      <Route path="about/" element={<About />} />
-      <Route path="contactus/" element={<ContactUs />} />
-      <Route path="help/" element={<Help />} />
-      <Route path="hirebike/" element={<HireBike />} />
-      <Route path="/bikeinfo/:id" element={<BikeInfo />} />
+      <Route element={<Layout/>}>
+        <Route path="GoCycle-Rental/" element={<HomePage />} />
+        <Route path="about/" element={<About />} />
+        <Route path="contactus/" element={<ContactUs />} />
+        <Route path="help/" element={<Help />} />
+        <Route path="hirebike/" element={<HireBike />} />
+        <Route path="/bikeinfo/:id" element={<BikeInfo />} />
+      </Route>
     </Routes>
   </BrowserRouter>
 )
