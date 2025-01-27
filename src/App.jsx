014f9a199792e131a40c1067cf4f413cbe6e9c4f
Silvia Saverino <silviasave89@gmail.com>
2025-01-27 16:52:18 +0000

    reorganise layout of About Us page. Add a navbar and used nested route and Outlet to render new nav page links. Edit style accordingly

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,28 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import "../server"
 import './App.css'
 import HomePage from './Pages/Home/Home.jsx'
-import About from './Pages/About/About.jsx'
+import AboutLayout from './Components/AboutLayout/AboutLayout.jsx'
+import AboutUs from './Pages/About/AboutUs.jsx'
 import ContactUs from './Pages/Contact Us/Contact.jsx'
 import Help from './Pages/Help/Help.jsx'
 import HireBike from './Pages/Hire Bike/Bikes.jsx'
 import BikeInfo from './Pages/Hire Bike/BikeInfo.jsx'
 import Layout from './Components/Layout/Layout.jsx'
+import Gallery from './Pages/Gallery/Gallery.jsx'
+import Reviews from './Pages/Reviews/Reviews.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter basename="/GoCycle-Rental">
     <Routes>
       <Route element={<Layout/>}>
         <Route path="GoCycle-Rental/" element={<HomePage />} />
-        <Route path="about/" element={<About />} />
+
+        <Route path="about" element={<AboutLayout />} >
+          <Route index element={<AboutUs />} />
+          <Route path="gallery" element={<Gallery />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+
         <Route path="contactus/" element={<ContactUs />} />
         <Route path="help/" element={<Help />} />
         <Route path="hirebike/" element={<HireBike />} />
---
+++ src/Components/AboutLayout/AboutLayout.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import { Link, Outlet } from "react-router-dom"
+import "./AboutLayout.css"
+
+export default function AboutLayout() {
+    return (
+        <>
+            <div className="about-header">
+                <h1>About <span>Go</span>Cycle Rental</h1>
+                <nav className="about-dashboard">
+                    <ul>
+                        <li><Link to="/about/">About Us</Link></li>
+                        <li> <Link to="/about/gallery">Gallery</Link></li>
+                        <li> <Link to="/about/reviews">Reviews</Link></li>
+                    </ul>
+                </nav>
+            </div>
+            <Outlet />
+        </>
+    )
+}
