e239c5a0ca9b6f1a18f251bbf8559cd948df886b
Silvia Saverino <silviasave89@gmail.com>
2025-01-30 16:53:11 +0000

    add initial style and footer layout

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,39 @@
+import React from "react"
+import "./Footer.css"
+
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <div className="footer-container">
+                <div classNameName="terms">
+                    <p>© 2024 GoCycle Rental. All rights reserved.</p>
+                    <p>
+                        <a href="#">Terms & Conditions</a> |
+                        <a href="#"> Privacy Policy</a>
+                    </p>
+                </div>
+                <div classNameName="contact-footer">
+                    <p>
+                        Contact us:</p>
+                    <p>
+                        <a href="mailto:info@gocyclerental.com">info@gocyclerental.com</a> |
+                        📞 123-456-7890
+                    </p>
+
+                </div>
+                <div className="social">
+                    <p>Follow us:</p>
+                    <p> <a href="#" target="_blank">Facebook</a>|
+                        <a href="#" target="_blank">Instagram</a>|
+                        <a href="#" target="_blank">Twitter</a>
+                    </p>
+
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+
+
+
