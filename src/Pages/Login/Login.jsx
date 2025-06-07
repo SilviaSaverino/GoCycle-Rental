d3f4c664f6cf42138d491c1766177eff9cc06a3d
Silvia Saverino <silviasave89@gmail.com>
2025-06-07 15:19:44 +0100

    add content and style login page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,12 +1,44 @@
 import React from "react"
-import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import "./Login.css"
 
 export default function Login() {
-    return (
-        <div className="login-form-container">
-            <h1>Login</h1>
-        </div>
+    const [loginFormData, setLoginFormData] = React.useState({ email: "", password: "" })
 
+    function handleSubmit(e) {
+        e.preventDefault()
+        console.log(loginFormData)
+    }
+
+    function handleChange(e) {
+        const { name, value } = e.target
+        setLoginFormData(prev => ({
+            ...prev,
+            [name]: value
+        }))
+    }
+
+    return (
+        <div className="login-container">
+            <h1>Log in to your account</h1>
+            <form onSubmit={handleSubmit} className="login-form">
+                <input
+                    name="email"
+                    onChange={handleChange}
+                    type="email"
+                    placeholder="Email address"
+                    value={loginFormData.email}
+                />
+                <input
+                    name="password"
+                    onChange={handleChange}
+                    type="password"
+                    placeholder="Password"
+                    value={loginFormData.password}
+                />
+                <button>Log in</button>
+            </form>
+        </div>
     )
+
 }
